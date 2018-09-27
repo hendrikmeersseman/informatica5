35bb936329b7a6d97b6b283f7c5737dda85a80f0
hendrikmeersseman <43027986+hendrikmeersseman@users.noreply.github.com>
2018-09-27 09:46:37 +0200

    Wet van Coulomb

---
+++ 04 - variabelen/2 - Wet van Coulomb.py
@@ -0,0 +1,11 @@
+q1 = 2.0 * (10** -6)
+q2 = 1.0 * (10** -6)
+k0 = 8.99 * (10 ** 9)
+
+#invoer
+r = float(input('geef afstand(in cm): '))
+
+#bewerking
+
+fc = k0 * (q1 * q2) / ((r * 10 ** -2) ** 2)
+print(fc)
