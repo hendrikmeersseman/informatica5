307ef02e0d5a775dd933ede89620cb089551fc61
hendrikmeersseman <43027986+hendrikmeersseman@users.noreply.github.com>
2018-10-09 21:54:40 +0200

    diatomist

---
+++ 05 - Eenvoudige functies+/2 - De diatomist.py
@@ -0,0 +1,13 @@
+#invoer
+rk = float(input('straal kleine cirkel: '))
+rg = float(input('straal grote cirkel: '))
+from math import pi
+
+#Berekenin
+n = int(0.83 * (rg ** 2) / (rk ** 2) - 1.9)
+opp_g_c = (rg * rg) * pi
+opp_k_c = (rk * rk) * pi
+procent = ((n * opp_k_c) / opp_g_c) * 100
+
+#uitvoer
+print('{} kleine cirkels bedekken {:.2f}% van de grote cirkel'.format(n, procent))
