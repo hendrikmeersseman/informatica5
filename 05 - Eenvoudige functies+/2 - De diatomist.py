84326557d30d14657488f0bc8fc22013507c1017
hendrikmeersseman <43027986+hendrikmeersseman@users.noreply.github.com>
2018-10-09 21:55:09 +0200

    diatomist

--- 05 - Eenvoudige functies+/2 - De diatomist.py
+++ 05 - Eenvoudige functies+/2 - De diatomist.py
@@ -5,8 +5,8 @@ from math import pi
 
 #Berekenin
 n = int(0.83 * (rg ** 2) / (rk ** 2) - 1.9)
-opp_g_c = (rg * rg) * pi
-opp_k_c = (rk * rk) * pi
+opp_g_c = (rg ** 2) * pi
+opp_k_c = (rk ** 2) * pi
 procent = ((n * opp_k_c) / opp_g_c) * 100
 
 #uitvoer
