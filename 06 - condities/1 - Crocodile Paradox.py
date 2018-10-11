7db159865fbea74eec3ac923f7afb8cd696dbedf
hendrikmeersseman <43027986+hendrikmeersseman@users.noreply.github.com>
2018-10-11 10:04:59 +0200

    krokodil

---
+++ 06 - condities/1 - Crocodile Paradox.py
@@ -0,0 +1,8 @@
+vraag = input('krokodil of vader: ')
+
+if vraag == 'vader':
+    print('Krokodil geeft kind terug')
+elif vraag == 'krokodil':
+    print('Krokodil eet kind op')
+else:
+    print('Moe van het denken')
