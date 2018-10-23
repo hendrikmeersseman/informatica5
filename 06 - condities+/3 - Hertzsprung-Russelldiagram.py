1a2af830bed9480d4b90e119d488abe8b2b0acbe
hendrikmeersseman <43027986+hendrikmeersseman@users.noreply.github.com>
2018-10-23 19:44:25 +0200

    Programma klaar

---
+++ 06 - condities+/3 - Hertzsprung-Russelldiagram.py
@@ -0,0 +1,20 @@
+#invoer
+temp = float(input('geef temperatuur van de ster: '))
+licht =float(input('Geef de lichtkrant van de ster: '))
+
+#berekening
+if licht > 10000:
+    uit = 'superreuzen (a)'
+elif licht > 1000:
+    uit = 'superreuzen (b)'
+elif licht > 100 and temp < 7500:
+    uit = 'heldere reuzen'
+elif licht > 10 and temp < 6000:
+    uit = 'reuzen'
+elif (licht < 0.01 and temp > 5000) or (licht < 0.0001 and temp > 3000):
+    uit = 'witte dwergen'
+else:
+    uit = 'hoofdreeks'
+
+#uitvoer
+print(uit)
