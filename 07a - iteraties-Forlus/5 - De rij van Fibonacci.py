af91153fe3e6a1afa7fbcdd4237941edc244aa0d
hendrikmeersseman <43027986+hendrikmeersseman@users.noreply.github.com>
2018-11-09 22:03:00 +0100

    Programma klaar

--- 07a - iteraties-Forlus/5 - De rij van Fibonacci.py
+++ 07a - iteraties-Forlus/5 - De rij van Fibonacci.py
@@ -1,7 +1,12 @@
 #invoer
 n = int(input('n = '))
-vuitv = 0
+uitv = 1
+v1uitv =0
 # rij van Fibonacci
-for i in range(n):
-    uitv = vuitv
-    uitv
+for i in range(n - 1):
+    v2uitv = v1uitv
+    v1uitv = uitv
+    uitv = v2uitv + v1uitv
+
+#uitvoer
+print (uitv)
