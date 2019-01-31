2fe5c7739e9564f7c242183e88a102f6fdb298c8
hendrikmeersseman <43027986+hendrikmeersseman@users.noreply.github.com>
2019-01-31 20:46:11 +0100

    Programma klaar

--- 11 -Tuple/03 - Vlaggen.py
+++ 11 -Tuple/03 - Vlaggen.py
@@ -1,3 +1,10 @@
-def vlag(invoer):
-    if invoer[0] == 'verticaal':
-        mes =
+def vlag(richtin, kleuren):
+    if richtin == 'verticaal':
+        mes = kleuren[0]
+        for i in range(1, len(kleuren)):
+            mes += ' | ' + kleuren[i]
+    else:
+        mes = kleuren[0]
+        for i in range(1, len(kleuren)):
+            mes += '\n-' + '\n' + kleuren[i]
+    return mes
