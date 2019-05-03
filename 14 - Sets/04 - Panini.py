a07a1fd9a2dbb59df1e40981813c02a67928b567
hendrikmeersseman <43027986+hendrikmeersseman@users.noreply.github.com>
2019-05-03 21:33:52 +0200

    Programma klaar

--- 14 - Sets/04 - Panini.py
+++ 14 - Sets/04 - Panini.py
@@ -1,12 +1,21 @@
 def nog_niet_in_bezit(nieuw, boek):
     return nieuw not in boek
 
+def controleren_wr_dubbel_is(dubbel)
 
 def verzamel(nieuw, boek, dubbel):
+
     if nog_niet_in_bezit(nieuw, boek):
         boek.add(nieuw)
         return boek, dubbel
 
+    if len(dubbel) == 0:
+        dubbel
+    stop, index = 0, 1
+    while not stop:
+
+
 
 print(verzamel('Bosmans',set(),{}))
 print(verzamel('Weber',{'Bosmans'},{}))
+print(verzamel('Bosmans',{'Weber', 'Bosmans'},{}))
