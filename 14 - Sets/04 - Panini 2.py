2eb6bfac77a1cc913f79d9bf0e403c0c30bfc8f9
hendrikmeersseman <43027986+hendrikmeersseman@users.noreply.github.com>
2019-05-08 21:27:39 +0200

    Programma klaar

--- 14 - Sets/04 - Panini 2.py
+++ 14 - Sets/04 - Panini 2.py
@@ -1,26 +1,23 @@
-def verzamel(kaart, boek, dubbel):
+def verzamel(nieuw, boek, dubbel):
     lengte = len(boek)
-    boek.add(kaart)
+    boek.add(nieuw)
     if len(boek) != lengte:
         return boek, dubbel
-    nieuw = {kaart}
-    verder = 0
+    verder = False
     for key, value in dubbel.items():
-        if nieuw.issubset(value):
-
-            sleutel = key
-            verder = 1
+        if {nieuw}.issubset(value):
+            sleutel, verder = key, True
     if verder:
-        dubbel[sleutel].remove(kaart)
+        dubbel[sleutel].remove(nieuw)
         if dubbel[sleutel] == set():
             dubbel.pop(sleutel)
         if sleutel + 1 in dubbel:
             dubbel[sleutel + 1].add(nieuw)
         else:
-            dubbel[sleutel + 1] = nieuw
+            dubbel[sleutel + 1] = {nieuw}
     else:
         if 1 in dubbel:
-            dubbel[1].add(kaart)
+            dubbel[1].add(nieuw)
         else:
-            dubbel[1] = nieuw
+            dubbel[1] = {nieuw}
     return boek, dubbel
